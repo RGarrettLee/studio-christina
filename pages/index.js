27e52c9e86ad04e64c65bdd446648a9b23f92776
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2023-11-26 17:57:52 -0500

    ended maintenance

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
         <h1 className='text-2xl font-playfair px-8 py-2 text-red-400 border-red-400 border-2 mt-12 lg:mt-24 mb-12'>christina lee</h1>
         <hr className='block mt-0.5 mb-0.5 mx-auto border border-red-400 w-3/4 xs:w-3/4 md:w-1/2' />
         <div className='flex flex-col items-center my-4'>
-          <h2 className='text-4xl font-playfair underline'>Under Maintenance</h2>
+          <ImageWall />
         </div>
     </div>
   )
